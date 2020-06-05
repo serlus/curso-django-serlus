f1108b69db38485f8caa6bb5c1bea7548d4de51f
serlus <serluscasas@gmail.com>
2020-06-05 10:53:04 -0300

    correction flake8

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -6,6 +6,7 @@ def test_status_code(client: Client):
     resp = client. get('/')
     assert resp.status_code == 200
 
+
 def test_title(client: Client):
     resp = client. get('/')
     assert_contains(resp, '<title>Web Design</title>')
